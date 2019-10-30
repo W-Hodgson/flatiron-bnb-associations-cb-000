743862a3e294710252906b262d84bb3e10d0327f
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 13:25:34 +0000

    Automatically backed up by Learn

--- db/migrate/20191030113936_create_reviews.rb
+++ db/migrate/20191030113936_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.0]
     create_table :reviews do |t|
       t.string :description
       t.integer :rating
+      t.integer :guest_id
 
       t.timestamps
     end
