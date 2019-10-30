4074f0cde16a8a9471aa9e17a9a7090e6ba7b5b0
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 13:24:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191030113936) do
+ActiveRecord::Schema.define(version: 20191030113415) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "title"
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 20191030113936) do
     t.float    "price"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string   "description"
-    t.integer  "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
