fdad7c538b6b97a1d19efe06a431d404f49fb948
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 14:24:23 +0000

    Automatically backed up by Learn

--- db/migrate/20191030142325_add_foreign_keys_to_tables.rb
+++ db/migrate/20191030142325_add_foreign_keys_to_tables.rb
@@ -2,5 +2,6 @@ class AddForeignKeysToTables < ActiveRecord::Migration[5.0]
   def change
     add_column :listings, :host_id, :integer
     add_column :listings, :neighborhood_id, :integer
+    
   end
 end
