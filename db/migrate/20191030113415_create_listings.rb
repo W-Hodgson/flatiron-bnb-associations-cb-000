679491aa56fa4c57df4251621cce36efb138bf91
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 13:25:11 +0000

    Automatically backed up by Learn

--- db/migrate/20191030113415_create_listings.rb
+++ db/migrate/20191030113415_create_listings.rb
@@ -6,6 +6,8 @@ class CreateListings < ActiveRecord::Migration[5.0]
       t.string :address
       t.string :listing_type
       t.float :price
+      t.integer :host_id 
+      t.integer :neighborhood_id
 
       t.timestamps
     end
