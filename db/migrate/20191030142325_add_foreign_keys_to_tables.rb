2333ce43ca1736aeef48f5652edefb5ffe3bebc4
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 14:25:13 +0000

    Automatically backed up by Learn

--- db/migrate/20191030142325_add_foreign_keys_to_tables.rb
+++ db/migrate/20191030142325_add_foreign_keys_to_tables.rb
@@ -4,5 +4,6 @@ class AddForeignKeysToTables < ActiveRecord::Migration[5.0]
     add_column :listings, :neighborhood_id, :integer
     add_column :neighborhoods, :city_id, :integer
     add_column :reservations, :guest_id, :integer
+    add_column :reservation
   end
 end
