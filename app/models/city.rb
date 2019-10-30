371258ac596e878be1286ae3290c46aa6d4968ee
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 14:08:12 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,4 @@
 class City < ApplicationRecord
-  has_many :neighborhoods, :class_name => :Neighborhood, :foreign_key => 'city_id'
+  has_many :neighborhoods, :foreign_key => 'city_id'
   has_many :listings, through: :neighborhoods
 end
