faba2b52b42d777173453682a1cd3544720dd829
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 14:11:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191030113415) do
-
-  create_table "listings", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "address"
-    t.string   "listing_type"
-    t.float    "price"
-    t.integer  "host_id"
-    t.integer  "neighborhood_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
+ActiveRecord::Schema.define(version: 20191030113011) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
