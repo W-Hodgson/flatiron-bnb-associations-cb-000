028b97ebc77ebb7dbe02e50bb8cd48e80c0befeb
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 13:26:55 +0000

    Automatically backed up by Learn

--- db/migrate/20191030115106_create_neighborhoods.rb
+++ db/migrate/20191030115106_create_neighborhoods.rb
@@ -2,6 +2,7 @@ class CreateNeighborhoods < ActiveRecord::Migration[5.0]
   def change
     create_table :neighborhoods do |t|
       t.string :name
+      t.integer :city_id
 
       t.timestamps
     end
