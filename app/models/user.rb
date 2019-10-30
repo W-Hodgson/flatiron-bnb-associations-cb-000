9164ecb5303f30410fcc6c403edacb7f94d235b0
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 13:30:50 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :listings, :foreign_key => "host_id"
   has_many :reviews, :foreign_key => "guest_id"
   has_many :reservations, through: :listings
+
+  has_many :trips
 end
