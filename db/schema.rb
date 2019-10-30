97bd59b561d4bc795dc7b3268699814d2f540c7f
Will Hodgson <will.hodgson@current-rms.com>
2019-10-30 14:12:30 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,12 +24,15 @@ ActiveRecord::Schema.define(version: 20191030115106) do
     t.string   "address"
     t.string   "listing_type"
     t.float    "price"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "host_id"
+    t.integer  "neighborhood_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "neighborhoods", force: :cascade do |t|
     t.string   "name"
+    t.integer  "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -37,6 +40,8 @@ ActiveRecord::Schema.define(version: 20191030115106) do
   create_table "reservations", force: :cascade do |t|
     t.datetime "checkin_time"
     t.datetime "checkout_time"
+    t.integer  "guest_id"
+    t.integer  "listing_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
@@ -44,8 +49,10 @@ ActiveRecord::Schema.define(version: 20191030115106) do
   create_table "reviews", force: :cascade do |t|
     t.string   "description"
     t.integer  "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
